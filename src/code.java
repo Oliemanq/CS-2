21d4221e8fec9a296bed20a9fe86c3f6c40ee6e6
Oliver Heisel <98408141+Oliemanq@users.noreply.github.com>
2025-01-17 14:39:24 -0600

    Added comments

--- src/code.java
+++ src/code.java
@@ -1,8 +1,10 @@
 public class code {
     public static void main(String[] args) {
 
+        //printing Hello, World!
         System.out.println("Hello, World!");
 
+        //printing numbers from 1 to 10
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
