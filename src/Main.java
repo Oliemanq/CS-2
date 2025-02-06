df3292303c9664ca5cdb354cf9114e65b9680e85
Oliemanq <oliemanq456@gmail.com>
2025-02-06 14:09:05 -0600

    Hello world print

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Lab3 test = new Lab3(10);
-
-        if (test == null){
-            System.out.println("Test is null");
-        }
+        System.out.println("Hello, World!");
     }
 }
 
