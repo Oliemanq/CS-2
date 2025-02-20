2534dd3aa00b82de019f12356f4221a4632c860f
Oliemanq <oliemanq456@gmail.com>
2025-02-20 14:16:16 -0600

    generates an arraylist of 1000 random ints from -1000 to 1000 and sorts it

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        ArrayList< Integer > data = new ArrayList<>();
+
+        // First need to create a PRNG engine
+        Random prng = new Random();
+
+        // generate 1000 elements between -1000 and 1000
+        int N = 1000;
+        for (int i=0; i<N; ++i) {
+            data.add( prng.nextInt(-1000, 1000) );
+        }
+
+        Collections.sort( data );
+
+        System.out.println("Amount of elements: " + data.size() );
+        for ( Integer n : data ) {
+            System.out.println( "Item: " + n );
+        }
 
     }
 }
